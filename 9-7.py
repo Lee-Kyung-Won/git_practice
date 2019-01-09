88d4df7ea7d0be1aca5ca08a07c4909f8410eadb
Lee-Kyung-Won <kunglee2545@gmail.com>
2019-01-09 15:17:17 +0900

    첫번째커밋

---
+++ 9-7.py
@@ -0,0 +1,14 @@
+in_file = open('chicekn.txt', 'r')
+
+day = 0
+
+for line in in_file:
+    data = line.strip().split(": ")
+    day += 1
+
+    for i in range(day):
+        amount += int(data[i])
+
+average = amount // day
+
+in_file.close()
